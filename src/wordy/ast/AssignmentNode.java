483b21fdd48cdcff382898f945ea5fe5e88db899
karlamartinez03 <kaitaely@gmail.com>
2026-09-21 20:27:00 -0500

    compileAssignment test passes

--- src/wordy/ast/AssignmentNode.java
+++ src/wordy/ast/AssignmentNode.java
@@ -2,6 +2,7 @@ package wordy.ast;
 
 import wordy.interpreter.EvaluationContext;
 
+import java.io.PrintWriter;
 import java.util.Map;
 import java.util.Objects;
 
@@ -62,5 +63,13 @@ public class AssignmentNode extends StatementNode {
     protected void doRun(EvaluationContext context){
         double value = expression.evaluate(context);
         context.set(variable.getName(),value);
+    }
+
+    @Override
+    public void compile(PrintWriter out){
+        variable.compile(out);
+        out.print(" = ");
+        expression.compile(out);
+        out.print(";");
     }
 }
